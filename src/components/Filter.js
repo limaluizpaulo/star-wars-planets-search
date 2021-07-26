4b2cc6a262ceeca53a2317c3301c86fd778d752b
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-07-26 03:25:56 -0300

    renderizado o filtro de coluna

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,19 +1,33 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import ApiContext from '../services/ApiContext';
 
 function Filter(){
   const { setFilters, filters } = useContext(ApiContext);
 
+  const [opColumn, setOpColumn] = useState(
+    ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'],
+  );
+  const [column, setColumn] = useState('population');
   const searchByText = ({ target: { value } }) => {
     setFilters({ ...filters, filterByName: { name: value.toLowerCase() } });
   };
 
+  const handle = ({ target: { value } }, callBack) => {
+    callBack(value);
+  };
+
   return(
-    <input
-    value={ filters.filterByName.name }
-    onChange={ searchByText }
-    data-testid="name-filter"
-  />
+    <section> 
+      <input
+      value={ filters.filterByName.name }
+      onChange={ searchByText }
+      data-testid="name-filter"
+    />
+
+      <select data-testid="column-filter" onChange={ (e) => handle(e, setColumn) }>
+        {opColumn.map((op) => (<option value={ op } key={ op }>{op}</option>))}
+      </select>
+    </section>
   )
 }
 
